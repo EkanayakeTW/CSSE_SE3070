a6a5e47e97843b26117a874a2009715350b4e703
Tiny Ekanayake <ekanayaketw@gmail.com>
2018-09-12 22:56:16 +0530

    UX created separately

---
+++ src/public_transport_ticketing_system/Public_Transport_Ticketing_System.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package public_transport_ticketing_system;
+
+/**
+ *
+ * @author Ekanayaketw
+ */
+public class Public_Transport_Ticketing_System {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
